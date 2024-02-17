b79cb9aac109e8104afc4abe90df298a5d3f9640
RZ9796 <rushizalte2001@gmail.com>
2024-02-17 17:21:13 +0530

    url commit

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ app.use(express.json());
 app.use(cors());
 app.use("/user", require("./routes/userRoutes"));
 
-// app.use(express.static(path.join(__dirname, "dist"))); //
+app.use(express.static(path.join(__dirname, "dist"))); //
 
-// app.use("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "dist", "index.html"));
-// });
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 mongoose.connect(process.env.MONGO_URL);
 mongoose.connection.once("open", () => {
